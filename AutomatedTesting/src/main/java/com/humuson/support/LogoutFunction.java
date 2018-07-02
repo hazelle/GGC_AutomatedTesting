7fd26f3be4723695e0046b9be47629a0f57f00e3
YangSeWan <tpdhks0369@gmail.com>
2018-07-02 15:35:44 +0900

    aaaaa

--- AutomatedTesting/src/main/java/com/humuson/support/LogoutFunction.java
+++ AutomatedTesting/src/main/java/com/humuson/support/LogoutFunction.java
@@ -1,17 +1,12 @@
 package com.humuson.support;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.humuson.selenium.StartTesting;
 
 /**
-<<<<<<< HEAD
- * 占싸그아울옙
-=======
- * 로그아웃
->>>>>>> 564511cdf5ddc160a9654db86be9ad4e6e56e939
+ * �α׾ƿ�
  */
 public class LogoutFunction extends Scenario {
 	protected static WebDriver driver = StartTesting.driver;
@@ -21,51 +16,22 @@ public class LogoutFunction extends Scenario {
 	protected static ControlPage cp = StartTesting.cp;
 
 	public LogoutFunction() {
-<<<<<<< HEAD
-		this.title = "*占싸그아울옙*";
-=======
-		this.title = "*로그아웃*";
->>>>>>> 564511cdf5ddc160a9654db86be9ad4e6e56e939
+		this.title = "*�α׾ƿ�*";
 		logout();
 	}
 
 	private void logout() {
 		if (cp.detectLogin()) {
-<<<<<<< HEAD
-			WebElement w = FEB("xpath", "//div[@id='header2']/div/div[2]/div/a[2]", "占쏙옙占쏙옙占쏙옙占� 占쏙옙疸濱占�");
-=======
-			WebElement w = FEB("xpath", "/html/body/header/header/div/div[2]/div/a[2]", "우측상단 드롭메뉴");
->>>>>>> 564511cdf5ddc160a9654db86be9ad4e6e56e939
+			WebElement w = FEB("xpath", "/html/body/header/header/div/div[2]/div/a[2]", "������� ��Ӹ޴�");
 			do {
 				w.click();
-				
-				try {
-					System.out.println(w.getAttribute("aria-expanded"));
-					Thread.sleep(1000);
-					
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				w = FEB("linkText", "로그아웃", "로그아웃");
-				System.out.println(w);
-				w.click();
 			} while (w.getAttribute("aria-expanded").equals("false"));
-<<<<<<< HEAD
-			w = FEB("linkText", "로그아웃", "로그아웃");
-			w.click();
-=======
-			FEB("xpath", "/html/body/header/header/div/div[2]/div/ul/li[10]/a", "로그아웃 메뉴").click();
->>>>>>> 564511cdf5ddc160a9654db86be9ad4e6e56e939
+			FEB("xpath", "/html/body/header/header/div/div[2]/div/ul/li[10]/a", "�α׾ƿ� �޴�").click();
 			
 			if(cp.checkPage("/login")) {
 				OK();
 			} else {
-<<<<<<< HEAD
-				FAIL("占싸그아울옙", driver.getCurrentUrl());
-=======
-				FAIL("로그아웃", driver.getCurrentUrl());
->>>>>>> 564511cdf5ddc160a9654db86be9ad4e6e56e939
+				FAIL("�α׾ƿ�", driver.getCurrentUrl());
 			}
 		}
 	}
